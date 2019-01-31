c45c79d2adcf7610551248ace1bb26c659e0ade6
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-01-31 08:52:59 +0100

    Acabado

---
+++ src/ejerciciocodsobrecarga/EjercicioCODSobrecarga.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocodsobrecarga;
+
+import static ejerciciocodsobrecarga.IO.variableInt;
+import static ejerciciocodsobrecarga.IO.variableFloat;
+import static ejerciciocodsobrecarga.IO.variableString;
+
+/**
+ *
+ * @author Samuel
+ */
+public class EjercicioCODSobrecarga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+     IO.introducir(variableString);
+     
+    }
+    
+}
